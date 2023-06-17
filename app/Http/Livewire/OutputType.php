2e71f50cdc48a89be1607087bf9263c6cb6256b1
shahy99 <shahenda.cs.99@gmail.com>
2023-06-17 10:47:06 +0200

    set function when choosing an input it takes the value to the next view

--- app/Http/Livewire/OutputType.php
+++ app/Http/Livewire/OutputType.php
@@ -9,9 +9,27 @@ use Livewire\Component;
 
 class OutputType extends Component
 {
+    public $query_list_data;
+    public $query_array_data;
+    public $query_table;
+    public $query_mix;
     public DbConnection $dbconnection;
     public ImportData $import_data;
     public AggregationFunction $aggregation_function;
+
+    public function next(){
+        if($this->query_list_data = 'query_list_data'){
+            return redirect()->route('query_list.index' , ['dbconnection' => $this->dbconnection->id , 'import_data' => $this->import_data->id , 'aggregation_function' => $this->aggregation_function->id]);
+        }else if($this->query_array_data = 'query_array_data'){
+            return redirect()->route('query_array.index' , ['dbconnection' => $this->dbconnection->id , 'import_data' => $this->import_data->id , 'aggregation_function' => $this->aggregation_function->id]);
+        }
+        else if($this->query_table){
+            return redirect()->route('query_table.index' , ['dbconnection' => $this->dbconnection->id , 'import_data' => $this->import_data->id , 'aggregation_function' => $this->aggregation_function->id]);
+        }
+        else if($this->query_mix){
+            return redirect()->route('query_mix.index' , ['dbconnection' => $this->dbconnection->id , 'import_data' => $this->import_data->id , 'aggregation_function' => $this->aggregation_function->id]);
+        }
+    }
     
     public function render()
     {
